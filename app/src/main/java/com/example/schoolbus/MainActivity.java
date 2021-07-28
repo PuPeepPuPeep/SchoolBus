bf4a7c67b07b68c67e153f135bc7e90bb724fa71
PuPeepPuPeep <cjsonastv@gmail.com>
2021-07-28 16:36:34 +0700

    Add Some Page

--- app/src/main/java/com/example/schoolbus/MainActivity.java
+++ app/src/main/java/com/example/schoolbus/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.schoolbus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnToRegister = (Button) findViewById(R.id.btnToRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+        btnToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegister();
+            }
+        });
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void openRegister(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/schoolbus/Register.java
@@ -0,0 +1,33 @@
+package com.example.schoolbus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+}
