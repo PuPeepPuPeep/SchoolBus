f1f815e64d28fa339c5e6c01ce226be1e28a72ad
PuPeepPuPeep <cjsonastv@gmail.com>
2021-07-28 17:38:54 +0700

    Add Menu

--- app/src/main/java/com/example/schoolbus/Driver.java
+++ app/src/main/java/com/example/schoolbus/Driver.java
@@ -3,6 +3,8 @@ package com.example.schoolbus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Driver extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Driver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/schoolbus/Transport.java
+++ app/src/main/java/com/example/schoolbus/Transport.java
@@ -3,6 +3,8 @@ package com.example.schoolbus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Transport extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Transport extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
