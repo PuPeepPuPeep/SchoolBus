903b408701c55b66f4f95a19b6f10b4edc47c958
PuPeepPuPeep <cjsonastv@gmail.com>
2021-07-28 17:23:24 +0700

    Add Driver Page

--- app/src/main/java/com/example/schoolbus/Home.java
+++ app/src/main/java/com/example/schoolbus/Home.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class Home extends AppCompatActivity {
 
     private Button btnTransport;
+    private Button btnDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btnTransport = (Button) findViewById(R.id.btnTransport);
+        btnDriver = (Button) findViewById(R.id.btnDriver);
 
         btnTransport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +26,22 @@ public class Home extends AppCompatActivity {
                 openTransport();
             }
         });
+
+        btnDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDriver();
+            }
+        });
     }
 
     public void openTransport(){
         Intent intent = new Intent(this, Transport.class);
+        startActivity(intent);
+    }
+
+    public void openDriver(){
+        Intent intent = new Intent(this, Driver.class);
         startActivity(intent);
     }
 }
