6b4fd77deedb46f474d6f3b21dd98590be67c4ab
PuPeepPuPeep <cjsonastv@gmail.com>
2021-08-15 02:41:41 +0700

    Fix StudentRecView

--- app/src/main/java/com/example/schoolbus/Home.java
+++ app/src/main/java/com/example/schoolbus/Home.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
